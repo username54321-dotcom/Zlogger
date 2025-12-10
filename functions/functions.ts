75bbc11c80ee0616db05afbd57774938092afd2f
username54321-dotcom <marwankam3l@gmail.com>
2025-12-10 13:30:22 +0200

    Full Rework

---
+++ functions/functions.ts
@@ -0,0 +1,62 @@
+// Constants
+let logs: Record<string, unknown>[] = [];
+let isSection = false;
+let timer: number = 0;
+
+// Start Time Tracking
+export function startTimer(): void {
+  timer === 0 && (timer = Date.now());
+}
+
+// Add a Log
+export function addLog<T>(value: T, iName: string): T {
+  // Log Object
+  const output = {
+    [iName]: {
+      value: value,
+      Duration: timer ? Date.now() - timer : "Timer was not Started",
+    },
+  };
+
+  // No Active Section -- Just Append
+  if (!isSection) {
+    logs.push(output);
+
+    // Active Section
+  } else if (isSection) {
+    const keptItems = logs.slice(0, logs.length - 1); // Items Before The Active Section
+    const sectionItem = logs.slice(-1)[0]; // Active Setion Object
+    const sectionName = Object.keys(sectionItem)[0];
+    const sectionValues = Object.values(sectionItem).flat(); // Previous Logs In Active Section
+    sectionValues.push(output); // Push The Log To Previous Logs
+    logs = [...keptItems, { [sectionName]: sectionValues }];
+  }
+  return value;
+}
+
+// Start a New Section
+export function addSection(sectionName: string) {
+  const newSection = { [sectionName]: [] };
+  logs.push(newSection);
+  isSection = true;
+}
+
+// End The Active Section
+export function endSection() {
+  isSection = false;
+}
+
+// Console Logging The Logs
+export function log() {
+  console.log(logs);
+}
+
+// Console.Table
+export function table() {
+  console.table(logs);
+}
+
+// Get Log
+export function getLogs() {
+  return logs;
+}
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-import * as functions from "./zustandStore.ts";
-export const addLog = functions.addLog;
-export const getLogs = functions.getLogs;
+import * as x from "./functions/functions.ts";
+
+export const startTimer = x.startTimer;
+export const addLog = x.addLog;
+export const addSection = x.addSection;
+export const startTendSectionimer = x.endSection;
+export const log = x.log;
+export const table = x.table;
+export const getLogs = x.getLogs;
