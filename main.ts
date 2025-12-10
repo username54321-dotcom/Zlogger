a0ee04840b3fcc6226e3009d08d6eba215d2a10f
username54321-dotcom <marwankam3l@gmail.com>
2025-12-10 05:55:49 +0200

    refactor: update exports in deno.json and simplify main.ts; remove unused files

--- main.ts
+++ main.ts
@@ -1,8 +1,2 @@
-import { addLog, getLogs } from "./zustandStore.ts";
-try {
-  const x = addLog("x", "value");
-  const y = addLog("userArray", [1, 2, 3, 4, 5, 6, 7]);
-  console.log(getLogs());
-} catch (error) {
-  console.log(getLogs());
-}
+import * as functions from "./zustandStore.ts";
+export const { addLog, getLogs } = functions;
