86fc2f58f85b49daf5b6ebf305699657edfc8335
username54321-dotcom <marwankam3l@gmail.com>
2025-12-11 10:13:39 +0200

    refactor: enhance Logger class with timer functionality

--- classes/main.ts
+++ classes/main.ts
@@ -1,28 +1,58 @@
 type Logs = Record<string, unknown>[];
 
+type TimerContents = {
+  "Start Timestamp": number;
+  "End Timestamp": string | number;
+  Duration: number | null;
+};
+type TimerObj = Record<string, TimerContents>;
+type TimerStack = Record<string, TimerObj>;
+
 export class Logger {
   logs: Logs = [];
   isSection: boolean = false;
-  timer: number = 0;
+  timestamp: number = performance.now();
+  timers: TimerStack = {};
 
   // Start Timer
-  startTimer() {
-    this.timer = performance.now();
+  startTimer(timerName: string) {
+    Object.defineProperty(this.timers, timerName, {
+      value: {
+        [timerName]: {
+          "Start Timestamp": performance.now(),
+          "End Timestamp": "Timer was not Stopped",
+          Duration: "N/A",
+        },
+      },
+    });
+  }
+
+  // End Timer
+  endTimer(timerName: string) {
+    const cTimestamp = performance.now();
+    const timerObj = Object.values(this.timers[timerName])[0];
+    Object.defineProperties(timerObj, {
+      "End Timestamp": {
+        value: cTimestamp,
+      },
+      Duration: {
+        value: +(cTimestamp - timerObj["Start Timestamp"]).toPrecision(3),
+      },
+    });
+    console.log(timerObj);
   }
 
   // Reset Timer
-  resetTimer() {
-    this.timer = 0;
-  }
+  // resetTimer() {
+  //   this.timer = 0;
+  // }
   // Add a Log
   addLog<T>(value: T, label: string): T {
     // Log Object
     const output = {
-      [label]: {
+      [`[ ${label} ]`]: {
         value: value,
-        Duration: this.timer
-          ? (performance.now() - this.timer).toPrecision(4) + " ms"
-          : "Timer was not Started",
+        TimeStamp: this.timestamp,
       },
     };
 
@@ -65,5 +95,14 @@ export class Logger {
   // Get Log
   getLogs(): typeof this.logs {
     return this.logs;
+  }
+  // Get Timers
+  getTimers(timerName?: string) {
+    if (!timerName) {
+      return this.timers;
+    } else {
+      const timerObj = Object.values(this.timers[timerName])[0];
+      return timerObj;
+    }
   }
 }
